88a70d1624a9a8dcd36c01fc3548200afe4612f4
jm786 <44728979+jm786@users.noreply.github.com>
2019-07-22 08:42:53 -0400

    Create installationscript.sh

---
+++ installationscript.sh
@@ -0,0 +1,9 @@
+# Dionaea
+wget "https://mhn.amnh.org/api/script/?text=true&script_id=17" -O deploy.sh && sudo bash deploy.sh https://mhn.amnh.org KxMFduan
+
+# SpoofSpotter
+wget "https://mhn.amnh.org/api/script/?text=true&script_id=18" -O deploy.sh && sudo bash deploy.sh https://mhn.amnh.org KxMFduan
+
+# Snort
+wget "https://mhn.amnh.org/api/script/?text=true&script_id=16" -O deploy.sh && sudo bash deploy.sh https://mhn.amnh.org KxMFduan
+
